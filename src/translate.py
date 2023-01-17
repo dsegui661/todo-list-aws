0348b783b138a57e52e03856057e20a6ce0120d9
Ubuntu <ubuntu@ip-172-31-9-70.ec2.internal>
2023-01-17 17:44:50 +0000

    Arreglar codigo para el pipeline

--- src/translate.py
+++ src/translate.py
@@ -3,23 +3,20 @@ import decimalencoder
 import todoList
 import boto3
 
+
 def translate(event, context):
     # create a response
     item = todoList.get_item(event['pathParameters']['id'])
     if item:
-        
-        #Translate item
         sourceLanguage = 'es'
         targetLanguage = event['pathParameters']['lang']
-        
-        trans = boto3.client(service_name='translate',region_name='us-east-1', use_ssl=True)
-        result = trans.translate_text(Text=item, SourceLanguageCode=sourceLanguage, TargetLanguageCode=targetLanguage)
-
+        trans = boto3.client(service_name='translate', region_name='us-east-1',
+                use_ssl=True)
+        result = trans.translate_text(Text=item, 
+                SourceLanguageCode=sourceLanguage, TargetLanguageCode=targetLanguage)
         print('TranslatedText: ' + result.get('TranslatedText'))
         print('SourceLanguageCode: ' + result.get('SourceLanguageCode'))
         print('TargetLanguageCode: ' + result.get('TargetLanguageCode'))
-        #End item translate
-        
         response = {
             "statusCode": 200,
             "body": json.dumps(result,
