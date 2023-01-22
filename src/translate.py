8eac3627f9a18d7e11a336bcde2ebe382a8ff6d2
Ubuntu <ubuntu@ip-172-31-9-70.ec2.internal>
2023-01-22 17:43:36 +0000

    Arreglar codigo para el pipeline

--- src/translate.py
+++ src/translate.py
@@ -11,10 +11,10 @@ def translate(event, context):
         sourceLanguage = 'es'
         targetLanguage = event['pathParameters']['lang']
         trans = boto3.client(service_name='translate', region_name='us-east-1',
-                            use_ssl=True)
+                             use_ssl=True)
         result = trans.translate_text(Text=item,
-                                    SourceLanguageCode=sourceLanguage,
-                                    TargetLanguageCode=targetLanguage)
+                                      SourceLanguageCode=sourceLanguage,
+                                      TargetLanguageCode=targetLanguage)
         print('TranslatedText: ' + result.get('TranslatedText'))
         print('SourceLanguageCode: ' + result.get('SourceLanguageCode'))
         print('TargetLanguageCode: ' + result.get('TargetLanguageCode'))
